9339672a02b19a5a945c156d3549d0b305de6715
dxl <dxl@britao.com>
2021-12-10 16:59:23 +0800

    customize 404 and homepage link

--- app.py
+++ app.py
@@ -7,9 +7,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'pubkey to address service'
+    return 'pubkey to address service, see <a href="https://plotnetwork.ltd">Plot Network Ltd</a> for more info'
+
+@app.errorhandler(404)
+def show_link():
+    return '404 - page not found. this only intend to API usage'
 
 @app.route('/pk2addr/<prefix>/<pk>')
 def pk2addr(prefix,pk):
-    return create_address_by_pk(pk,prefix)
+    try:
+        addr = create_address_by_pk(pk,prefix)
+    except:
+        addr = 'err'
+    return addr
+
 
