be83e3756141d5de55f5811e58a27fa11fa30013
dxl <dxl@britao.com>
2021-12-11 10:54:31 +0800

    support multiple addr convert in single POST

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
 #!/usr/bin/env python3
 
 from cvt import create_address_by_pk
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'pubkey to address service, see <a href="https://plotnetwork.ltd">Plot Network Ltd</a> for more info'
 
-@app.errorhandler(404)
-def show_link():
-    return '404 - page not found. this only intend to API usage'
-
-@app.route('/pk2addr/<prefix>/<pk>')
-def pk2addr(prefix,pk):
+def convert(prefix,pk):
     try:
         addr = create_address_by_pk(pk,prefix)
     except:
         addr = 'err'
     return addr
 
+@app.route('/')
+def index():
+    return 'pubkey to address service, see <a href="https://plotnetwork.ltd">Plot Network Ltd</a> for more info'
 
+@app.errorhandler(404)
+def show_link(url):
+    return '404 - page not found. this only intend to API usage'
+
+@app.route('/pk2addr/<prefix>/<pk>')
+def pk2addr(prefix,pk):
+    return convert(prefix,pk)
+
+@app.route('/pk2addr/<prefix>', methods=['POST'])
+def pk2addrs(prefix):
+    rd = request.get_json()
+    res = {}
+    if 'pks' in rd:
+        addrs = []
+        for pk in rd['pks']:
+            addrs.append(convert(prefix,pk))
+        res['addrs'] = addrs
+
+    return jsonify(res)
